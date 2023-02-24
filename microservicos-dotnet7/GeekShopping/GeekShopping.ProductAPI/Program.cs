4c341e2662f6f128f50b4b33510ee687a714dedc
Leandro Lorente <lorenteleandro@hotmail.comgit config --global user.name Leandro>
2023-02-24 16:23:42 -0300

    #[feat] - adicionando algumas camadas novas

--- microservicos-dotnet7/GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ microservicos-dotnet7/GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,3 +1,7 @@
+using GeekShopping.ProductAPI.Model.Context;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8,0,32)))); 
 
 var app = builder.Build();
 
