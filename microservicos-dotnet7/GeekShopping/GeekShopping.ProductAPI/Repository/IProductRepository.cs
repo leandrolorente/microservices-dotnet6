84177911ed7d7cd4d5e8e3727f1c1093abf0747e
Leandro Lorente <lorenteleandro@hotmail.comgit config --global user.name Leandro>
2023-03-16 20:50:12 -0300

    #criação de interface para comunicação com as entidades.

--- microservicos-dotnet7/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ microservicos-dotnet7/GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.DTO;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductDto>> FindAll();
+        Task<ProductDto> FindById(long id);
+        Task<ProductDto> FindByName(string name);
+        Task<ProductDto> Create(ProductDto dto);
+        Task<ProductDto> Update(ProductDto dto);
+        Task<ProductDto> DeleteById(long id);
+        Task<ProductDto> DeleteByName(string name);
+        
     }
 }
