464418896f6b3961019e9590696587d93120e853
leandrolorente <lorenteleandro@hotmail.com>
2023-07-05 11:27:50 -0300

    #section11 [ feat] - criação do identity server da aplicação

--- microservicos-dotnet7/GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ microservicos-dotnet7/GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,9 +1,41 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connection = builder.Configuration["MySQlConnection:MySQlConnectionString"];
+
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 32))));
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySqlContext>()
+    .AddDefaultTokenProviders();
+
+var build = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+
+}).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+  .AddInMemoryClients(IdentityConfiguration.Clientes)
+  .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+  .AddAspNetIdentity<ApplicationUser>();
+
+build.AddDeveloperSigningCredential();
+
+builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
+
+//var initializer = app.Services.CreateScope().ServiceProvider.GetService<IDbInitializar>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -17,6 +49,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseIdentityServer();
 
 app.UseAuthorization();
 
